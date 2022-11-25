88369c202a48f28eb770997d3aa3ac26e49a7f1f
Brandon Scott <BrandonScott615@gmail.com>
2022-11-25 18:00:11 -0500

    update with code

--- myFlask/flaskTest.py
+++ myFlask/flaskTest.py
@@ -2,5 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():#This handles application URLS, known as view functions, the return value is the **response** the client receives
+    return '<h>Hello World!</h1>'
+
+app.add_url_rule('/', 'index', index)
+
 
 
